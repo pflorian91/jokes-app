9da68fc0585b393a1aa541c529a42395343399cd
Florian Popa <fpopa1991@gmail.com>
2021-02-24 07:12:38 +0200

    Initial commit

---
+++ src/main/java/com/webgenerals/jokesapp/controller/JokesController.java
@@ -0,0 +1,15 @@
+package com.webgenerals.jokesapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * JokesController
+ *
+ * @author Florian Popa fpopa1991@gmail.com
+ */
+@Controller
+public class JokesController {
+
+  
+
+}
